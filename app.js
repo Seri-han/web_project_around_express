67a780663abd9362fc4a8ea97aa8efcd63d2a379
Sarah Handal <sarah.handal@gmail.com>
2025-07-13 18:19:28 -0600

    preparando el backend

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
-// eslint-disable-next-line import/no-extraneous-dependencies
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/users');
 const cardRoutes = require('./routes/cards');
 
 const app = express();
 const PORT = 3000;
+
+const { errors } = require('celebrate');
 
 app.use(express.json());
 
@@ -25,6 +27,32 @@ mongoose.connect('mongodb://127.0.0.1:27017/aroundb')
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Recurso solicitado no encontrado' });
+});
+
+app.use(requestLogger);
+
+app.post('/signup', createUser);
+app.post('/signin', login);
+app.use('/users', usersRouter);
+app.post('/posts', postsRouter);
+
+app.use(errorLogger);
+
+app.use(errors()); // Manejo de errores de Celebrate
+
+// Middleware para rutas no encontradas
+app.use((req, res, next) => {
+  const error = new Error('Recurso solicitado no encontrado');
+  error.statusCode = 404;
+  next(error);
+});
+
+// Middleware genérico para manejo de errores
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).json({
+    message: statusCode === 500 ? 'Error interno del servidor' : message,
+  });
 });
 
 app.listen(PORT, () => {
